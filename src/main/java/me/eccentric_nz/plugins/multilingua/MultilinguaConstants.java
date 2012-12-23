13598ad4219fd1cefc03977001423ccbf64f5de4
eccentric devotion <eccentricdevotion@home.nz>
2012-12-23 18:02:55 +1300

    Add characters and a method to shuffle them

--- src/main/java/me/eccentric_nz/plugins/multilingua/MultilinguaConstants.java
+++ src/main/java/me/eccentric_nz/plugins/multilingua/MultilinguaConstants.java
@@ -5,4 +5,22 @@ import org.bukkit.ChatColor;
 public class MultilinguaConstants {
 
     public static final String MY_PLUGIN_NAME = ChatColor.GOLD + "[Multi-lingua] " + ChatColor.RESET;
+    public static final String[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,:;!?()0123456789 []@$%&".split("");
+    public static final String cipher = "MNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.,:;!?()0123456789 []@$%&ABCDEFGHIJKL";
+
+    public static String shuffle(String string) {
+        StringBuilder sb = new StringBuilder(string.length());
+        double rnd;
+        for (char c : string.toCharArray()) {
+            rnd = Math.random();
+            if (rnd < 0.34) {
+                sb.append(c);
+            } else if (rnd < 0.67) {
+                sb.insert(sb.length() / 2, c);
+            } else {
+                sb.insert(0, c);
+            }
+        }
+        return sb.toString();
+    }
 }
