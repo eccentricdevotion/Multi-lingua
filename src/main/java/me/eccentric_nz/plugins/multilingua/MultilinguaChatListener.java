caa794790e99e11c87b772c273d6bc9825d2bc25
eccentric devotion <eccentricdevotion@home.nz>
2012-12-23 18:04:12 +1300

    Cipher chat if the recieving player is not in the same faction

--- src/main/java/me/eccentric_nz/plugins/multilingua/MultilinguaChatListener.java
+++ src/main/java/me/eccentric_nz/plugins/multilingua/MultilinguaChatListener.java
@@ -1,5 +1,8 @@
 package me.eccentric_nz.plugins.multilingua;
 
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
@@ -7,6 +10,8 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 public class MultilinguaChatListener implements Listener {
 
     private Multilingua plugin;
+    public static final String DEFAULT_ENCODING = "UTF-8";
+    private final String key = "[" + plugin.getConfig().getString("plain_text_key") + "]";
 
     public MultilinguaChatListener(Multilingua plugin) {
         this.plugin = plugin;
@@ -14,6 +19,34 @@ public class MultilinguaChatListener implements Listener {
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent event) {
-
+        String chat = event.getMessage();
+        String[] first = chat.split(" ");
+        String[] chars = chat.split("");
+        StringBuilder sb = new StringBuilder();
+        for (String c : chars) {
+            if (plugin.encoder.containsKey(c)) {
+                sb.append(plugin.encoder.get(c));
+            } else {
+                sb.append(c);
+            }
+        }
+        String cipher = sb.toString();
+        FPlayer fp = FPlayers.i.get(event.getPlayer());
+        String faction = fp.getFactionId();
+        for (Player p : plugin.getServer().getOnlinePlayers()) {
+            FPlayer onlineP = FPlayers.i.get(p);
+            if ((onlineP.hasFaction() && onlineP.getFactionId().equals(faction)) || first[0].equals(key)) {
+                // send the plain message
+                if (first[0].equals(key)) {
+                    int len = chat.length();
+                    chat = chat.substring(3, len);
+                }
+                p.sendMessage(chat);
+            } else {
+                // send the cipher message
+                p.sendMessage(cipher);
+            }
+        }
+        event.setCancelled(true);
     }
 }
